20f513a59146766628104ea121748757c4ca75e8
BHJ14946 <106410578+BHJ14946@users.noreply.github.com>
2022-07-06 09:51:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var box
+function setup() 
+{
+  createCanvas(400, 400);
+box=new Box(100,200,100,100,2,1)
+
+
+
+}
+
+function draw() 
+
+
+
+
+{
+  background(220);
+box.show()
+box.movey()
+box.move()
+
+
+}
+
